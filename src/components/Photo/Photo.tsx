c96d40dbec67a26083f324c7afbabbf19d6d09a4
christopher lassota <christopherlassota@outlook.com>
2024-11-26 15:13:45 -0700

    completed footer and added dynamic content to photo cards(accidentally in wrong branch

--- src/components/Photo/Photo.tsx
+++ src/components/Photo/Photo.tsx
@@ -1,15 +1,25 @@
 import "./Photo.scss";
 
-const Photo = () => {
+interface PhotoProps {
+  name: string;
+  imageUrl: string;
+  tags: string[];
+}
+
+const Photo = ({ name, imageUrl, tags }: PhotoProps) => {
   return (
-    <article className="photo">
-      <div className="photo__name">Greg Hooper</div>
+    <article
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+      className="photo"
+    >
+      <div className="photo__name">{name}</div>
       <div className="photo__tagbar">
         <ul className="photo__taglist">
-          <li className="photo__tag">New York</li>
-          <li className="photo__tag">Bridge</li>
-          <li className="photo__tag">Skyline</li>
-          <li className="photo__tag">Cars</li>
+          {tags.map((tag) => (
+            <li key={Photo.id}className="photo__tag">{tag}</li>
+          ))}
         </ul>
       </div>
     </article>
--- src/components/PhotoGallery/PhotoGallery.tsx
+++ src/components/PhotoGallery/PhotoGallery.tsx
@@ -1,10 +1,18 @@
 import "./PhotoGallery.scss";
 import Photo from "../Photo/Photo";
+import photos from "../../data/photos.json";
 
 const PhotoGallery = () => {
   return (
     <section className="photogallery">
-      <Photo />
+      {photos.map((photo) => (
+        <Photo
+          key={photo.id}
+          name={photo.photographer}
+          imageUrl={photo.photo}
+          tags={photo.tags}
+        />
+      ))}
     </section>
   );
 };
