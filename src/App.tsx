20c3e14a73feeb6232153dd47f184c516eb03ce1
christopher lassota <christopherlassota@outlook.com>
2024-11-29 17:03:40 -0700

    add filter tag selection functionality and selected tag styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./styles/App.scss";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
@@ -6,11 +7,35 @@ import Footer from "./components/Footer/Footer";
 import FilterMenu from "./components/FilterMenu/FilterMenu";
 
 const App = () => {
+  /**
+   * Toggle FilterMenu drop down
+   * Change styling of filtermenu button when toggled on
+   */
+  const [filterMenuToggle, setFilterMenuToggle] = useState(false);
+  const [buttonState, setButtonState] = useState("");
+  
+  const handleFilterClick = () => {
+    setFilterMenuToggle(!filterMenuToggle);
+    buttonState === ""
+    ? setButtonState("--selected")
+    : setButtonState("");
+  };
+
+  /**
+   * Select a tag
+   */
+  const [selectedTag, setSelectedTag] = useState("");
+  const handleTagClick = (tag:string) => {
+    selectedTag === tag
+    ? setSelectedTag("")
+    : setSelectedTag(tag)
+  }
+  
   return (
     <>
-      <Header />
+      <Header filterClick={handleFilterClick} buttonState={buttonState}/>
       <main className="main">
-        <FilterMenu />
+        {filterMenuToggle ? <FilterMenu handleTagClick={handleTagClick} selectedTag={selectedTag}/> : null}
         <Hero />
         <PhotoGallery />
       </main>
--- src/components/FilterMenu/FilterMenu.tsx
+++ src/components/FilterMenu/FilterMenu.tsx
@@ -1,18 +1,25 @@
 import "./FilterMenu.scss";
 import tagsData from "../../data/tags.json";
 
+interface FilterMenuProps {
+  handleTagClick: (tag:string) => void;
+  selectedTag: string;
+}
+           
+// selectedTag === tag ? filter__tag--selected : ""
 
-
-const FilterMenu = () => {
-
+const FilterMenu = ({ handleTagClick, selectedTag }: FilterMenuProps) => {
   return (
     <section className="filter">
       <h2 className="filter__title">Filters</h2>
       <ul className="filter__tag-list">
         {tagsData.map((tag, index) => (
           <li
+            onClick={() => {
+              handleTagClick(tag);
+            }}
             key={index}
-            className="filter__tag"
+            className={selectedTag === tag ? "filter__tag filter__tag--selected" : "filter__tag"}
           >
             {tag}
           </li>
