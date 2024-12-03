2ee14af8d2dd6051fcbf6e5a5ab3944adc7de982
christopher lassota <christopherlassota@outlook.com>
2024-12-02 19:16:13 -0700

    create photo details page

--- src/pages/PhotoDetails/PhotoDetails.tsx
+++ src/pages/PhotoDetails/PhotoDetails.tsx
@@ -5,12 +5,13 @@ import axios from "axios";
 
 const PhotoDetails = () => {
   // {"api_key":"a90b1dab-b8ae-4277-9879-c9bcfe3201c3"}
+  
 
   const [photoData, setPhotoData] = useState([])
 
   useEffect(() => {
     const fetchPhotos = async () => {
-        const response = await axios.get('https://unit-3-project-c5faaab51857.herokuapp.com/photos/6be619d5-de2c-4f62-ac1b-bd2e36d71ab9?api_key=a90b1dab-b8ae-4277-9879-c9bcfe3201c3');
+        const response = await axios.get(`https://unit-3-project-c5faaab51857.herokuapp.com/photos/{$photo.id}?api_key=a90b1dab-b8ae-4277-9879-c9bcfe3201c3`);
         setPhotoData(response.data)
     }
 
