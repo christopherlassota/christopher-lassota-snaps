f803bd0129c95ecdaa41e679dcf0529ba3502d4f
christopher lassota <christopherlassota@outlook.com>
2024-12-02 18:42:04 -0700

    create error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.scss";
 import HomePage from "./pages/HomePage/HomePage";
+import ErrorPage from "./pages/ErrorPage/ErrorPage.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,7 @@
+import './ErrorPage.scss';
+
+const ErrorPage = () => {
+    return <p>Error 404 Page not Found</p>
+}
+
+export default ErrorPage;
