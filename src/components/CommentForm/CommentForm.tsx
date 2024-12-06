354b67fde8dd9236b1ffbb528de1605910a8a450
christopher lassota <christopherlassota@outlook.com>
2024-12-06 15:35:25 -0700

    add post-comment functionality

--- src/components/CommentForm/CommentForm.tsx
+++ src/components/CommentForm/CommentForm.tsx
@@ -1,36 +1,71 @@
+import { SyntheticEvent, useState } from "react";
 import "./CommentForm.scss";
-// import { useParams } from "react-router-dom";
-// import axios from "axios";
-// import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const CommentForm = () => {
-  //   const params = useParams();
-  //   useEffect(() => {
-  //     const postComment = async (comment) => {
-  //       try {
-  //         const response = await axios.post(
-  //           `https://unit-3-project-c5faaab51857.herokuapp.com/photos/${params.id}/comments?api_key=a90b1dab-b8ae-4277-9879-c9bcfe3201c3`,
-  //           comment,
-  //           { headers: { "Content-Type": "application/json" } }
-  //         );
-  //       } catch (error) {
-  //         console.log("Failed to post comment:", error);
-  //       }
-  //     };
-  //   });
+  const params = useParams();
+  const [formData, setFormData] = useState({
+    name: "",
+    comment: "",
+  });
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const postComment = async (comment: { name: string; comment: string }) => {
+    try {
+      const response = await axios.post(
+        `https://unit-3-project-c5faaab51857.herokuapp.com/photos/${params.id}/comments?api_key=a90b1dab-b8ae-4277-9879-c9bcfe3201c3`,
+        comment,
+        { headers: { "Content-Type": "application/json" } }
+      );
+      console.log("Comment posted successfully:", response.data);
+    } catch (error) {
+      console.log("Failed to post comment:", error);
+    }
+  };
+
+  const handleFormSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+    postComment(formData);
+    setFormData({ name: "", comment: "" });
+  };
 
   return (
     <section className="new-comment">
-      <form action="submit" className="new-comment__form">
+      <form
+        action="submit"
+        className="new-comment__form"
+        onSubmit={handleFormSubmit}
+      >
         <label htmlFor="" className="new-comment__label">
           Name
         </label>
-        <input type="text" className="new-comment__name" />
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          className="new-comment__name"
+          onChange={handleInputChange}
+        />
         <label htmlFor="" className="new-comment__label">
           Comment
         </label>
-        <textarea type="text" className="new-comment__text" />
-        <div className="new-comment__button">Submit</div>
+        <textarea
+          name="comment"
+          value={formData.comment}
+          className="new-comment__text"
+          onChange={handleInputChange}
+        />
+        <button type="submit">Submit</button>
       </form>
     </section>
   );
